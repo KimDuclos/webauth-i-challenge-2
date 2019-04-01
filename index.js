81ebdb796e7dc8195ca6120a7963307de3ffe1d0
Kim Duclos <kimduclos@gmail.com>
2019-04-01 13:25:24 -0700

    working on index.js

--- index.js
+++ index.js
@@ -1,6 +1,59 @@
 const bcrypt = require('bcryptjs');
 
 // hash a pw
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+
+const db = require('./database/dbConfig');
+const Users = require('./users/users-model');
+
+const server = express();
+
+server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+server.get('/', (req, res) => {
+    res.send('Johnny 5 is ALIVE!')
+});
+
+server.post('/api/register', (req, res) => {
+    let user = req.body;
+
+    Users.add(user)
+    .then(saved => {
+        res.status(201).json(error);
+    });
+});
+
+server.post('api/login', (req, res) => {
+    let { username, password } = req.body;
+
+    Users.findby({ username })
+    .first()
+    .then(user => {
+        if (user) {
+            res.status(200).json({ message: `Come on in, ${user.username}!`})
+        }
+        else
+        res.status(401).json({ message: 'Did you fat-finger your creds?'});
+    })
+    .catch(error => {
+        res.status(500).json(error);
+    });
+});
+
+server.get('api/users', (req, res) => {
+    Users.find()
+        .
+})
+
+
+
+
 const credentials = req.body;
 const hash = bcrypt.hashSync(credentials, password, 14);
 
